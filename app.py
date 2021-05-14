0f9f89b91d7dc0ce5222abbb0472b0bb44d4e343
Belendia Serda <belendia@gmail.com>
2021-05-14 20:59:28 +0300

    Add code to download data

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
 from dhis2 import Api
 import json
+from os.path import exists
 
-SERVER = 'https://'
+SERVER = ''
 USERNAME = ''
 PASSWORD = ''
 
 ORG_UNIT_FIELDS = 'id,name,shortName,featureType,coordinates,parent'
 PAGE_SIZE = 50
 
+DATA_ELEMENTS = {"rmqxJ1TtUEA": "B50-196 PF",
+                 "moq6FPoeBDm": "B50-197 PF with cerebral complications",
+                 "ITGJpcL9wra": "B50-198 Other severe and complicated PF",
+                 "z1mnnlpCKew": "B50-199 PF unspecified",
+                 "SnKf3dD3W5T": "B50-200 PV",
+                 "ALQjhqZF6LA": "B50-201 PV with other complications",
+                 "xb597ZmVbCq": "B50-202 PV without complication",
+                 "WvMqItWTzvZ": "B50-203 Mixed (Other parasitologically confirmed malaria)",
+                 "GYZmBo0O4ui": "B50-204 Unspecified malaria"}
+
 
 class DHIS2:
     def __init__(self):
         self.api = Api(SERVER, USERNAME, PASSWORD)
+        self.__dataset_id = 'LNLZYbrGEh6'
         self.org_units = {}
         self.__org_unit_file_name = 'metadata/OrgUnits.json'
 
@@ -32,15 +44,40 @@ class DHIS2:
         if 'pager' in self.org_units:
             del self.org_units['pager']
 
+    def __download_data(self):
+        print('Downloading org units...')
+        for index, org_unit in enumerate(self.org_units['organisationUnits']):
+            r = self.api.get('dataValueSets', params={
+                'dataSet': [self.__dataset_id],
+                'orgUnit': ['FZN1YXK7fWW'],
+                'startDate': '2010-01-01',
+                'endDate': '2013-09-01'
+            })
+
+            if index == 30:
+                exit()
+            print(r.json())
+            exit()
+
     def __save_org_units(self):
         print('Saving org units to {} ... '.format(self.__org_unit_file_name), end=" ")
         with open(self.__org_unit_file_name, 'w') as f:
             json.dump(self.org_units, f)
         print('Done')
 
+    def __load_json_file(self):
+        print('Loading {} ... '.format(self.__org_unit_file_name), end=" ")
+        with open(self.__org_unit_file_name, "r") as json_file:
+            self.org_units = json.load(json_file)
+        print('Done')
+
     def run(self):
-        self.__download_org_units()
-        self.__save_org_units()
+        if exists(self.__org_unit_file_name):
+            self.__load_json_file()
+        else:
+            self.__download_org_units()
+            self.__save_org_units()
+        self.__download_data()
 
 
 if __name__ == "__main__":
